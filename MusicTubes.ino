08c4a12810c9889771baf8f234755861ccb38617
Zverrr <zverrr@skbkontur.ru>
2018-07-27 04:00:27 +0500

    Player manager. Support 7 buttons (from pin 2 to 8)
    Pin 2 = Stop
    Pins 3-7 play files 0001 to 0006

--- MusicTubes.ino
+++ MusicTubes.ino
@@ -1,39 +1,107 @@
 #include <DFRobotDFPlayerMini.h>
 #include <SoftwareSerial.h>
 
+#define ISSTOPPED  0
+#define ISPLAYING  1
+#define FIRST_BUTTON_PIN 2
+#define BUTTON_COUNT 7
+
+class PlayerStateManager
+{
+  private:
+  byte _currentState = 0;
+  byte _previousNonZeroState = 0;
+  DFRobotDFPlayerMini* _player;
+  byte _hopCounter = 0;
+  
+  public:
+  PlayerStateManager(DFRobotDFPlayerMini *player);
+  void Initialize();
+  void ReadState();
+  void ProcessState();
+  void CheckStop();
+};
+
+SoftwareSerial softSerial(13,12);
 DFRobotDFPlayerMini myDFPlayer;
-const int buttonStart = 2;
-const int buttonStop = 3;
-
-int buttonStartState = 0;
-int buttonStopState = 0;
-
-bool nowPlaying = 0;
+PlayerStateManager manager(&myDFPlayer);
 
 void setup() {
-  pinMode(buttonStart, INPUT);
-  pinMode(buttonStop, INPUT);
-  Serial.begin(9600);
-  myDFPlayer.begin(Serial);
-  delay(1000);
-  myDFPlayer.volume(25);
-  
+  manager.Initialize();
 }
 
 void loop() {
-  
   // put your main code here, to run repeatedly:
-  buttonStartState = digitalRead(buttonStart);
-  if(buttonStartState == HIGH && !nowPlaying)
-  {
-    myDFPlayer.play(1);
-    nowPlaying = true;
-  }
-  buttonStopState = digitalRead(buttonStop);  
-  if(buttonStopState == HIGH && nowPlaying)
-  {
-    myDFPlayer.stop();
-    nowPlaying = false;
-  }
+  
+  manager.ReadState();
+  manager.ProcessState();
+  manager.CheckStop();
   delay(100);  
+}
+
+PlayerStateManager::PlayerStateManager(DFRobotDFPlayerMini *player)
+{
+  _player = player;
+}
+
+void PlayerStateManager::Initialize()
+{
+  for(byte button = FIRST_BUTTON_PIN; button < FIRST_BUTTON_PIN + BUTTON_COUNT; button++)
+  {
+    pinMode(button, INPUT);
+  }
+  softSerial.begin(9600);
+  _player->begin(softSerial);
+//  Serial.begin(115200); //For debug
+  delay(100);
+  _player->volume(20);
+}
+
+void PlayerStateManager::ReadState()
+{
+  int buttonState;
+  _currentState = 0;
+  for(byte button = FIRST_BUTTON_PIN; button < FIRST_BUTTON_PIN + BUTTON_COUNT; button++)
+  {
+    buttonState = digitalRead(button);
+    byte bitNumber = button - FIRST_BUTTON_PIN;
+    byte mask = buttonState << bitNumber;
+    _currentState|=mask;  
+  }
+}
+
+void PlayerStateManager::ProcessState()
+{
+  if(!_currentState || _currentState == _previousNonZeroState)
+    return;  
+  int pushedButtonNumber = 0;  
+  int i = 0;
+  while(i <= BUTTON_COUNT)
+  {
+    if(_currentState&(1 << i))
+      pushedButtonNumber = i;
+    i++;
+  }
+  if(pushedButtonNumber)
+    _player->play(pushedButtonNumber);
+  else
+    _player->stop();  
+  
+  _previousNonZeroState = _currentState;  
+}
+
+void PlayerStateManager::CheckStop()
+{
+  //проверяем раз в 5 сек, иначе тормозит
+  if(_hopCounter < 50)
+  {
+    _hopCounter++;
+    return;
+  }
+  int state = _player->readState()&0xf;
+  if(state == ISSTOPPED && _previousNonZeroState != 1) 
+  {
+   _previousNonZeroState = 1;
+  }
+  _hopCounter = 0;
 }
