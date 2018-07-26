9a5405dea2d9fed04056f4ceafe70fc2a1a7cb91
Zverrr <zverrr@skbkontur.ru>
2018-07-27 00:56:55 +0500

    Version for two buttons

--- MusicTubes.ino
+++ MusicTubes.ino
@@ -2,18 +2,38 @@
 #include <SoftwareSerial.h>
 
 DFRobotDFPlayerMini myDFPlayer;
+const int buttonStart = 2;
+const int buttonStop = 3;
+
+int buttonStartState = 0;
+int buttonStopState = 0;
+
+bool nowPlaying = 0;
 
 void setup() {
+  pinMode(buttonStart, INPUT);
+  pinMode(buttonStop, INPUT);
   Serial.begin(9600);
   myDFPlayer.begin(Serial);
   delay(1000);
   myDFPlayer.volume(25);
-  myDFPlayer.play(1);
+  
 }
 
 void loop() {
   
   // put your main code here, to run repeatedly:
-  delay(30000);
-  myDFPlayer.next();
+  buttonStartState = digitalRead(buttonStart);
+  if(buttonStartState == HIGH && !nowPlaying)
+  {
+    myDFPlayer.play(1);
+    nowPlaying = true;
+  }
+  buttonStopState = digitalRead(buttonStop);  
+  if(buttonStopState == HIGH && nowPlaying)
+  {
+    myDFPlayer.stop();
+    nowPlaying = false;
+  }
+  delay(100);  
 }
